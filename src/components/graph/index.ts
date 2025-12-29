3995261c3c04755ab3065424d48132ac6eb8b3ad
Sakeeb91 <rahman.sakeeb@gmail.com>
2025-12-29 09:36:58 -0500

    feat(graph): export coverage components from barrel
    Add CoverageHeatmap and EvidenceDrawer to graph components exports
    for easier importing throughout the application.

--- src/components/graph/index.ts
+++ src/components/graph/index.ts
@@ -6,3 +6,5 @@ export { KnowledgeGraph } from './KnowledgeGraph';
 export { GraphVisualization } from './GraphVisualization';
 export { GraphControls } from './GraphControls';
 export { NodeDetailsPanel } from './NodeDetailsPanel';
+export { CoverageHeatmap } from './CoverageHeatmap';
+export { EvidenceDrawer } from './EvidenceDrawer';
