372335e73c24ad643ee589a6cf9d40c796b06dbc
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-01-02 22:48:46 -0500

    feat(collaboration): export new components from barrel export
    Add CollaborationPanel (with StackedAvatars, EditingAlert), ShareDialog,
    and PermissionsManager to the collaboration components barrel export.
    Enables clean imports: import { ShareDialog } from '@/components/collaboration';

--- src/components/collaboration/index.ts
+++ src/components/collaboration/index.ts
@@ -7,6 +7,10 @@ export { CollaborativeEditor } from './CollaborativeEditor';
 export { CommentThread } from './CommentThread';
 export { VersionHistory } from './VersionHistory';
 // export { LiveCursors } from './LiveCursors'; // TODO: Rename UserCursor to LiveCursors
-// export { CollaborationPanel } from './CollaborationPanel'; // TODO: Create component
-// export { ShareDialog } from './ShareDialog'; // TODO: Create component
-// export { PermissionsManager } from './PermissionsManager'; // TODO: Create component
+export {
+  CollaborationPanel,
+  StackedAvatars,
+  EditingAlert,
+} from './CollaborationPanel';
+export { ShareDialog } from './ShareDialog';
+export { PermissionsManager } from './PermissionsManager';
