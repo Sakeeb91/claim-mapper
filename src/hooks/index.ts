fb31e4bf849f609970ef59135a3cb1991e4e6078
Sakeeb91 <rahman.sakeeb@gmail.com>
2026-01-02 22:56:07 -0500

    feat(hooks): export useReasoningChainState from hooks index
    Add barrel export for reasoning chain state hook with type exports:
    - ReasoningChainState interface
    - UseReasoningChainStateReturn interface
    - UseReasoningChainStateOptions interface

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -8,5 +8,11 @@ export { useCoverage, useStepEvidence } from './useCoverage';
 export { useDebounce } from './useDebounce';
 export { useLocalStorage } from './useLocalStorage';
 export { useGraphExport } from './useGraphExport';
+export { useReasoningChainState } from './useReasoningChainState';
+export type {
+  ReasoningChainState,
+  UseReasoningChainStateReturn,
+  UseReasoningChainStateOptions,
+} from './useReasoningChainState';
 // export { useGraph } from './useGraph'; // TODO: Create useGraph hook
 // export { useCollaboration } from './useCollaboration'; // TODO: Create useCollaboration hook
